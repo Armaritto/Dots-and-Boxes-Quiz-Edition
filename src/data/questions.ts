5f3f9f460e74eee79430d72f1e423df04d62008d
Fadi Sarwat <fadisarwat@yahoo.com>
2024-12-17 12:47:27 +0200

    Change all urls to use .env

--- src/data/questions.ts
+++ src/data/questions.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { Question } from '../types';
 
-const API_URL = 'http://localhost:9000/api';
+const API_URL = import.meta.env.VITE_API_URL + '/api';
 
 export const fetchQuestions = async (teamIds: number[]): Promise<Record<number, Question[]>> => {
   try {
