fbecb4bed908e17bbd60e137bfbaadc1e0798962
Fadi Sarwat <fadisarwat@yahoo.com>
2024-12-17 11:38:55 +0200

    Update cors

--- server.js
+++ server.js
@@ -4,7 +4,10 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 
 const app = express();
-app.use(cors());
+app.use(cors({
+    origin: process.env.VITE_API_URL,
+    optionsSuccessStatus: 200
+}));
 app.use(express.json());
 dotenv.config();
 
